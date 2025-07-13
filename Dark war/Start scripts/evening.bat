0fc6c840ed72feddc1c9ad5821db57e3145dd7e9
TechFred <TechFred@users.noreply.github.com>
2025-07-12 22:36:00 -0400

    Shutdown scripts for Dark war

--- Dark war/Start scripts/evening.bat
+++ Dark war/Start scripts/evening.bat
@@ -4,8 +4,16 @@ net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo Running as admin...
     powershell -Command "Start-Process '%~dp0..\run.exe' -ArgumentList 'evening.json' -Verb RunAs"
+		::  "Start-Process '%~dp0..\run.exe' -ArgumentList 'night.json' -Verb RunAs -Wait"
     exit /b
 )
 
 :: Already admin, run the command
 "%~dp0..\run.exe" evening.json
+
+
+:: Wait for a moment (optional)
+timeout /t 3 >nul
+
+:: Put the computer to sleep
+rundll32.exe powrprof.dll,SetSuspendState 0,1,0

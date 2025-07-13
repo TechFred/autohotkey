fe21db22e94b48a36e967421598813647e495c08
TechFred <TechFred@users.noreply.github.com>
2025-07-13 11:56:07 -0400

    Ajustement bat

--- Dark war/Start scripts/day.bat
+++ Dark war/Start scripts/day.bat
@@ -4,7 +4,6 @@ net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo Running as admin...
     powershell -Command "Start-Process '%~dp0..\run.exe' -ArgumentList 'day.json' -Verb RunAs"
-		::  "Start-Process '%~dp0..\run.exe' -ArgumentList 'night.json' -Verb RunAs -Wait"
     exit /b
 )
 
@@ -17,3 +16,5 @@ timeout /t 3 >nul
 
 :: Put the computer to sleep
 rundll32.exe powrprof.dll,SetSuspendState 0,1,0
+
+:: "Start-Process '%~dp0..\run.exe' -ArgumentList 'night.json' -Verb RunAs -Wait"
